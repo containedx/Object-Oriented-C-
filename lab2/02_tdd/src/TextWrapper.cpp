c358bf875a958dbc17269b705e22e82cc4ba2413
kingazx <niu.ula.a@gmail.com>
2019-10-16 17:33:15 +0200

    [red] draft of test

--- lab2/02_tdd/src/TextWrapper.cpp
+++ lab2/02_tdd/src/TextWrapper.cpp
@@ -11,9 +11,5 @@ int TextWrapper::columns()
 
 std::string  TextWrapper::wrap(std::string str)
 {
-   //std::string t = str.at(0) + "\n" ;
-   std::string t = "a\nb";
-   std::cout << str.at(0) << "\n" << str.at(1) << "\n";
-   std:: cout << t;
-   return t;
+   return str;
 }
