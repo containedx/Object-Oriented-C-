37b0e54d0dff6b0078267546d351db7129292368
kinga <niu.ula.a@gmail.com>
2019-11-11 12:10:42 +0100

    small bench

--- lab5/bench/src/SmallBench.cpp
+++ lab5/bench/src/SmallBench.cpp
@@ -2,3 +2,19 @@
 #include "BenchIncludes.h"
 
 // TODO: Add benchmarks for operator<. operator==, and hash
+
+static void Operator1( State& state )
+{
+    for (auto _ : state)
+    {
+        Small A{};
+        Small B{};
+
+        auto operation = A < B;
+
+        DoNotOptimize(&state);
+    }
+    state.SetComplexityN(state.range(0));
+}
+
+BENCHMARK( Operator1 )->RangeMultiplier(2)->Range(1<<10, 1<<18)->Complexity();
