7532033d4a6c63cc28f4b90ab266b7141cbda1c4
kinga <niu.ula.a@gmail.com>
2020-01-13 19:33:50 +0100

    Displacement, Speed & Acceleration Tests

--- 12_qt_testing/fromlab11/fromlab11.h
+++ 12_qt_testing/fromlab11/fromlab11.h
@@ -2,6 +2,9 @@
 #define FROMLAB11_H
 
 #include "fromlab11_global.h"
+#include "acceleration.h"
+#include "displacement.h"
+#include "speed.h"
 
 class FROMLAB11SHARED_EXPORT Fromlab11
 {
--- 12_qt_testing/tests/Fromlab11Test/tst_test1.cpp
+++ 12_qt_testing/tests/Fromlab11Test/tst_test1.cpp
@@ -1,4 +1,5 @@
 #include <QtTest>
+#include "fromlab11.h"
 
 // add necessary includes here
 
@@ -10,15 +11,82 @@ public:
 
 
 private slots:
-    void test_case1();
+    void DisplacementTest();
+    void SpeedTest();
+    void AccelerationTest();
 
 };
 
-
-void Test1::test_case1()
-{
   //signal spy czy sygnaly wyemitowane i spawdzic argymenty
   //set property i property
+
+
+void Test1::DisplacementTest()
+{
+    Displacement D1{};
+    QCOMPARE(1, D1.get());
+
+    QSignalSpy spy(&D1, SIGNAL(changed(double)));
+
+    double v = rand()%100;
+    D1.set(v);
+    QCOMPARE(v, D1.get());
+
+    QCOMPARE( 1, spy.count());
+
+    double x = rand()%100;
+    D1.set(x);
+    QCOMPARE(x, D1.get());
+
+    QCOMPARE( 2, spy.count());
+}
+
+void Test1::SpeedTest()
+{
+    Speed S1{};
+    QCOMPARE(S1.get(), NULL);
+
+    QSignalSpy spy(&S1, SIGNAL(changed(double)));
+
+    double v = rand()%40;
+    S1.onChange(v);
+    QCOMPARE(S1.get(), v);
+
+    double x = rand()%50;
+    S1.onChange(x);
+    QCOMPARE(S1.get(), x - v);
+
+    QCOMPARE(2, spy.count());
+
+    double z = rand()%15;
+    S1.onChange(z);
+    QCOMPARE(S1.get(), z - x);
+
+    QCOMPARE(3, spy.count());
+}
+
+void Test1::AccelerationTest()
+{
+    Acceleration A1{};
+    QCOMPARE(A1.get(), NULL);
+
+    QSignalSpy spy(&A1, SIGNAL(changed(double)));
+
+    double v = rand()%5;
+    A1.onChange(v);
+    QCOMPARE(A1.get(), v);
+
+    double x = rand()%9;
+    A1.onChange(x);
+    QCOMPARE(A1.get(), x - v);
+
+    QCOMPARE(2, spy.count());
+
+    double z = rand()%8;
+    A1.onChange(z);
+    QCOMPARE(A1.get(), z - x);
+
+    QCOMPARE(3, spy.count());
 }
 
 QTEST_APPLESS_MAIN(Test1)
